fcb41e4d51cc35f79253b0e906258903c04fdebd
manlikeked1 <donaldkene123@gmail.com>
2020-06-18 15:49:44 +0100

    added a drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,7 +1,8 @@
 //This whole screen represents the homepage of the application.
 
 import 'package:flutter/material.dart';
-// import 'package:meal_app/screens/favorites_page.dart';
+
+import '../widgets/main_drawer.dart';
 
 import '../screens/favorites_page.dart';
 import '../screens/categories_page.dart';
@@ -13,14 +14,21 @@ class TabsScreen extends StatefulWidget {
 
 class _TabsScreenState extends State<TabsScreen> {
   final List<Map<String, dynamic>> _pages = [
+    //List o widgets that you want to render in each tab
     {'page': CategoriesPage(), 'title': 'Categories'},
-    {'page': FavoritesPage(), 'title': 'Favorites Section'}
+    {
+      'page': FavoritesPage(),
+      'title': 'Favorites Section',
+    }
   ];
 
-  int _selectedPageIndex = 0;
+  int _selectedPageIndex =
+      0; //THis is a property which is an integer which is '0' initially
 
   void _selectPage(int index) {
+    //This is the method that 'ontap' listener uses
     setState(() {
+      //I called setState which is why I used a stateful widget instead of a stateless widget. The setState is used because we will manually switch that index
       _selectedPageIndex = index;
     });
   }
@@ -32,22 +40,30 @@ class _TabsScreenState extends State<TabsScreen> {
         appBar: AppBar(
           title: Text(_pages[_selectedPageIndex]['title']),
         ),
+        drawer:
+            MainDrawer(), //Here, I placed the drawer in the 'tabs screen' because this the defaukt screen
         body: _pages[_selectedPageIndex]['page'],
         bottomNavigationBar: BottomNavigationBar(
-            onTap: _selectPage,
+            //This is what a tab bar at the bottom is called
+            //For the 'bottomNavigationBar' you manually have to control what the user selected and which content you want to display
+            onTap:
+                _selectPage, //This is an 'onTap' listener and it is triggered whenever an item is selected
             backgroundColor: Theme.of(context).primaryColor,
             unselectedItemColor: Colors.white,
             selectedItemColor: Theme.of(context).accentColor,
-            currentIndex: _selectedPageIndex,
+            currentIndex:
+                _selectedPageIndex, //This tells the bottomNavigationBar which tab is actually selected
             selectedFontSize: 15,
             unselectedFontSize: 10,
             // type: BottomNavigationBarType.shifting,//For animation
             items: [
               BottomNavigationBarItem(
+                //This is just a tab
                 backgroundColor: Theme.of(context)
                     .primaryColor, //Used when adding the animation 'shifting'
-                icon: Icon(Icons.category),
-                title: Text('Categories'),
+                icon: Icon(Icons.category), //An Icon
+                title: Text(
+                    'Categories'), //The title now is a widget and not a plain text
               ),
               BottomNavigationBarItem(
                 backgroundColor: Theme.of(context)
@@ -55,34 +71,33 @@ class _TabsScreenState extends State<TabsScreen> {
                 icon: Icon(Icons.star),
                 title: Text('Favorites'),
               )
-            ])
-
-        // This is for creating a tab bar that is creatred at the top of an application
-        // return DefaultTabController(
-        //This is responsible for adding a 'Tab Bar' at the top of the project, just beneath the appbar
-        // length:
-        // 2, //This helps us to defiine the amount of tabs that we want in the project. Hence the number 2, which means that we want 2 tabs
-        // initialIndex: 0,
-        // child: Scaffold(
-        //       appBar: AppBar(
-        //           title: Text('Meals'),
-        //           bottom: TabBar(tabs: <Widget>[
-        //             Tab(
-        //                 icon: Icon(Icons.category),
-        //                 text:
-        //                     'Categories'), //This is responsible for the Categories tab. This feature is an inbuilt feature in flutter
-        //             Tab(
-        //                 icon: Icon(Icons.star),
-        //                 text:
-        //                     'Favorites'), //This is responsible for the Favorites tab. This feature is an inbuilt feature in flutter
-        //           ])),
-        //       body: TabBarView(children: <Widget>[
-        //         CategoriesPage(),
-        //         FavoritesPage()
-        //       ]), //Note: Always arrange the screens in the way in which you want them to be arranged. Let the arrangement here correspond with yur arrangement in yoor 'tab()' section above
-        //     ),
-        //   );
-        // }
-        );
+            ]));
   }
 }
+
+// This is for creating a tab bar that is creatred at the top of an application
+// return DefaultTabController(
+//This is responsible for adding a 'Tab Bar' at the top of the project, just beneath the appbar
+// length:
+// 2, //This helps us to defiine the amount of tabs that we want in the project. Hence the number 2, which means that we want 2 tabs
+// initialIndex: 0,
+// child: Scaffold(
+//       appBar: AppBar(
+//           title: Text('Meals'),
+//           bottom: TabBar(tabs: <Widget>[
+//             Tab(
+//                 icon: Icon(Icons.category),
+//                 text:
+//                     'Categories'), //This is responsible for the Categories tab. This feature is an inbuilt feature in flutter
+//             Tab(
+//                 icon: Icon(Icons.star),
+//                 text:
+//                     'Favorites'), //This is responsible for the Favorites tab. This feature is an inbuilt feature in flutter
+//           ])),
+//       body: TabBarView(children: <Widget>[
+//         CategoriesPage(),
+//         FavoritesPage()
+//       ]), //Note: Always arrange the screens in the way in which you want them to be arranged. Let the arrangement here correspond with yur arrangement in yoor 'tab()' section above
+//     ),
+//   );
+// }
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  //Using a builder method to create ListTiles
+  Widget buildListTile(String title, IconData icon) {
+    //Here we passed two arguments into the builder, 'title' and 'icon'
+    return ListTile(
+      leading: Icon(
+        icon, //Here, we placed the 'icon' argumrnt
+        size: 24,
+        color: Colors.white, //This is the size of the icon
+      ),
+      title: Text(
+        title, //Here we placed the 'title' argument
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white),
+      ),
+      onTap: () {
+        // ...
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120, //This gives the height of the container
+            width: double
+                .infinity, //This allows the container to take the full width of the screen
+            padding: EdgeInsets.all(20), //This gives a padding of 20 all around
+            alignment: Alignment.bottomCenter,
+            // color: Theme.of(context).accentColor,//This gives the background color to the container
+            decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(
+                    10)), //This is a feature on flutter that helps you to add more than one form of decoration
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(height: 25), //This helps to give a spacing
+          //Using the 'buildListTile' method to pass in 'meals' for the first ListTile and 'icons.restaurant' for the second ListTile
+          buildListTile(
+            'Meals',
+            Icons.restaurant,
+          ),
+          buildListTile('Filters', Icons.settings),
+        ],
+      ),
+    );
+  }
+}
